beeb1a18cab03c662fea6e37c8738a4094ea2fc2
hemani0606 <hemanisaravanan2011@gmail.com>
2026-09-16 22:26:55 -0700

    Add print statement for demonstration
    Added a new print statement for demonstration.

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
     System.out.println("Hello world!");
 
     //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
-
+System.out.println("Hello")
     //ANSWER: 
 
     //This is my 3rd period change
